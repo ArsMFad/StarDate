5eec1582095a4381051659a3c64044bbf8800ced
ArsMFad <arsmfad@gmail.com>
2020-04-25 16:43:29 +0300

    This is where the fun begins

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let days_in_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+let new_year = 17;
+
+
